a28e60bd875af33341a66af144b70bfad27bc286
Thắng <thangsaberhero@gmail.com>
2026-05-29 01:29:26 +0700

    sửa env backend

--- back-end-do-an/config/db.js
+++ back-end-do-an/config/db.js
@@ -6,9 +6,13 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // <--- THÊM DÒNG NÀY VÀO ĐÂY NHÉ!
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Test kết nối ngay khi khởi động
