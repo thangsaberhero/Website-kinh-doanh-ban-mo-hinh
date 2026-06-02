898fd30d7a9660cc8a4a0117734191ab9f2d9ac9
vuphong2109 <vuhongphong04@gmail.com>
2026-06-03 01:12:40 +0700

    Cập nhật phần đơn hàng ở trang dashboard và quản lý đơn hàng

--- back-end-do-an/routes/Admin_route/donhang_admin.route.js
+++ back-end-do-an/routes/Admin_route/donhang_admin.route.js
@@ -19,19 +19,16 @@ router.get('/:MaDH', authMiddleware.verifyToken, authMiddleware.verifyStaff, don
 // 2. NHÓM TẠO MỚI DỮ LIỆU (POST)
 // ==========================================
 router.post('/add', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.tao_don_hang_ngoai);
-
-// ĐỔI TỪ PUT SANG POST: Để khớp 100% với hàm fetch() ở Frontend Vue.js
 router.post('/update', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.cap_nhat_trang_thai_don_hang);
 router.post('/huy', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.huy_don_hang);
 
 // ==========================================
 // 3. NHÓM CẬP NHẬT/THAY ĐỔI DỮ LIỆU (PUT)
 // ==========================================
-// Đổi về verifyStaff để nhân viên trực page có thể sửa địa chỉ cho khách
 router.put('/fix', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.sua_thong_tin_don_hang);
 router.put('/hoan_hang', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.hoan_tra_don_hang);
 
-// API Mới: Xác nhận thanh toán (Thu tiền COD, Chuyển khoản...)
-//router.put('/payment-status', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.xac_nhan_thanh_toan);
+router.put('/payment-status', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.xac_nhan_thanh_toan);
+router.put('/refund-status', authMiddleware.verifyToken, authMiddleware.verifyStaff, donhang_admin_Controller.xac_nhan_hoan_tien);
 
 module.exports = router;
