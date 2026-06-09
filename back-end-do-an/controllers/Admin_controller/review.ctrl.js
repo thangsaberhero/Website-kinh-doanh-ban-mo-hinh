f3cadbfac7357bf8d5c2879be9e3eac759114c0b
Thắng <thangsaberhero@gmail.com>
2026-06-09 18:02:16 +0700

    Sửa link ảnh

--- back-end-do-an/controllers/Admin_controller/review.ctrl.js
+++ back-end-do-an/controllers/Admin_controller/review.ctrl.js
@@ -80,7 +80,7 @@ const adminReviewController = {
                 if (item.HinhAnh && item.HinhAnh !== '[]' && item.HinhAnh !== 'null') {
                     try {
                         const imgArr = typeof item.HinhAnh === 'string' ? JSON.parse(item.HinhAnh) : item.HinhAnh;
-                        parsedImages = imgArr.map(img => `http://localhost:3000/Images_review/${img}`);
+                        parsedImages = imgArr; 
                     } catch (e) { 
                         console.error("Lỗi parse JSON ảnh đánh giá:", e); 
                     }
