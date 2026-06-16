801be93938b303b4d614615183ac0a1143f6af32
Thắng <thangsaberhero@gmail.com>
2026-06-17 01:23:01 +0700

    Sửa

--- front-end-do-an/src/views/Admin_view/AdminInventory.vue
+++ front-end-do-an/src/views/Admin_view/AdminInventory.vue
@@ -287,7 +287,7 @@
                           <div class="flex flex-col">
                             
                             <p @click="openEditModal(product)" 
-                              class="font-bold text-sky-600 text-sm line-clamp-2 hover:text-sky-700 hover:underline cursor-pointer transition-colors" 
+                              class="font-bold text-sky-600 text-sm line-clamp-2 whitespace-normal hover:text-sky-700 hover:underline cursor-pointer transition-colors" 
                               title="Bấm để xem và sửa chi tiết sản phẩm">
                               {{ product.name }}
                             </p>
